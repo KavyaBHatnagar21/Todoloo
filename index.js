e5c4e3d789948aea70249e7555768747373022c3
Kavya Bhatnagar <bhatnagarkavya16@gmail.com>
2024-12-18 22:07:54 +0530

    implemented delete functionality and enter to add task

--- index.js
+++ index.js
@@ -28,15 +28,48 @@ const retrieveItems = () => {
   todoList.innerHTML = ""
   tasks.forEach((task, index) => {
     todoList.innerHTML += `
-      <div class="box-and-item-container">
-       <img src="./images/${
-         task.completed ? "checked-box.png" : "unchecked-box.png"
-       }" class="box-img" data-index="${index}" alt="check-boxes">
-    <li class="${task.completed ? "completed" : ""}">${task.text}</li>
-      <div>`
+    <div class="item-container">
+        <div class="box-and-item-container">
+            <img src="./images/${
+              task.completed ? "checked-box.png" : "unchecked-box.png"
+            }" class="box-img" data-index="${index}" alt="check-boxes">
+            <li class="${task.completed ? "completed" : ""}">${task.text}</li>
+        </div>
+        <img src="./images/bin.png" class="bin-img hidden" data-index="${index}" alt="bin icon">
+    </div>
+      `
   })
 
+  displayBinOnHover()
+  deleteTask()
   checkTheBox()
+}
+
+const deleteTask = () => {
+  const binImg = document.querySelectorAll(".bin-img")
+
+  binImg.forEach((img) => {
+    img.addEventListener("click", function () {
+      const index = img.dataset.index
+      tasks.splice(index, 1)
+      localStorage.setItem("tasks", JSON.stringify(tasks))
+      retrieveItems()
+    })
+  })
+}
+
+const displayBinOnHover = () => {
+  const itemContainers = document.querySelectorAll(".item-container")
+  itemContainers.forEach((item) => {
+    const binImg = item.querySelector(".bin-img")
+
+    item.addEventListener("mouseover", () => {
+      binImg.classList.remove("hidden")
+    })
+    item.addEventListener("mouseout", () => {
+      binImg.classList.add("hidden")
+    })
+  })
 }
 
 const checkTheBox = () => {
@@ -44,7 +77,6 @@ const checkTheBox = () => {
 
   boxImg.forEach((img) => {
     img.addEventListener("click", function () {
-      console.log("I am being clicked!")
       const index = img.dataset.index
       tasks[index].completed = !tasks[index].completed
       localStorage.setItem("tasks", JSON.stringify(tasks))
@@ -56,4 +88,11 @@ const checkTheBox = () => {
 addButton.addEventListener("click", () => {
   addItems()
   retrieveItems()
+})
+
+inputBox.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    addItems()
+    retrieveItems()
+  }
 })
